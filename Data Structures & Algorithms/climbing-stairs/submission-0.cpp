428964db6a90322277c2abfa017c754b1d307da3
Himanshu Kumar <himanshu3729k@gmail.com>
2026-07-25 18:01:17 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int ans = 0;
+        int dp[46] = {0};
+        dp[n] = 1, dp[n-1] = 2;
+
+        for(int i=n-2;i>=0;i--){
+            dp[i] = dp[i+1]+dp[i+2];
+        }
+
+        return dp[1];
+    }
+};
